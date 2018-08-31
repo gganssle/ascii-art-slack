1e3a0394a58f4fcc02ebcc801212f69628e066c8
gigagram <graham.ganssle@experoinc.com>
2018-08-30 19:10:36 -0500

    wrapping up flask

--- app.py
+++ app.py
@@ -3,21 +3,24 @@ import numpy as np
 import requests
 from pyfiglet import Figlet
 
+fonts = ['3-d', '3x5', '5lineoblique', 'acrobatic', 'alligator', 'alligator2', 'alphabet', 'avatar', 'banner', 'banner3-D', 'banner3', 'banner4', 'barbwire', 'basic', 'bell', 'big', 'bigchief', 'binary', 'block', 'bubble', 'bulbhead', 'calgphy2', 'caligraphy', 'catwalk', 'chunky', 'coinstak', 'colossal', 'computer', 'contessa', 'contrast', 'cosmic', 'cosmike', 'cricket', 'cursive', 'cyberlarge', 'cybermedium', 'cybersmall', 'diamond', 'digital', 'doh', 'doom', 'dotmatrix', 'drpepper', 'eftichess', 'eftifont', 'eftipiti', 'eftirobot', 'eftitalic', 'eftiwall', 'eftiwater', 'epic', 'fender', 'fourtops', 'fuzzy', 'goofy', 'gothic', 'graffiti', 'hollywood', 'invita', 'isometric1', 'isometric2', 'isometric3', 'isometric4', 'italic', 'ivrit', 'jazmine', 'jerusalem', 'katakana', 'kban', 'larry3d', 'lcd', 'lean', 'letters', 'linux', 'lockergnome', 'madrid', 'marquee', 'maxfour', 'mike', 'mini', 'mirror', 'mnemonic', 'morse', 'moscow', 'nancyj-fancy', 'nancyj-underlined', 'nancyj', 'nipples', 'ntgreek', 'o8', 'ogre', 'pawp', 'peaks', 'pebbles', 'pepper', 'poison', 'puffy', 'pyramid', 'rectangles', 'relief', 'relief2', 'rev', 'roman', 'rot13', 'rounded', 'rowancap', 'rozzo', 'runic', 'runyc', 'sblood', 'script', 'serifcap', 'shadow', 'short', 'slant', 'slide', 'slscript', 'small', 'smisome1', 'smkeyboard', 'smscript', 'smshadow', 'smslant', 'smtengwar', 'speed', 'stampatello', 'standard', 'starwars', 'stellar', 'stop', 'straight', 'tanja', 'tengwar', 'term', 'thick', 'thin', 'threepoint', 'ticks', 'ticksslant', 'tinker-toy', 'tombstone', 'trek', 'tsalagi', 'twopoint', 'univers', 'usaflag', 'wavy', 'weir']
+
 app = Flask(__name__)
 
 @app.route('/hello')
 def hello_world():
     return 'Hello World!'
 
-@app.route('/v20180830', methods = ['GET'])
-def api():
+@app.route('/v20180830/<input>', methods = ['GET'])
+def api(input):
     if request.method == 'GET':
+        font = np.random.choice(fonts)
+        f = Figlet(font=font)
 
-        dictionary = {'var1':request.args.get('arg1'),
-                      'var2':request.args.get('arg2')}
-    return jsonify(dictionary)
+    return f.renderText(input)
+
 # to access the above, use:
-# http://127.0.0.1:5000/API?arg1=One&arg2=Two
+# curl http://127.0.0.1:5000/v20180830/Hi.%20Youre%20awesome!
 
 if __name__ == '__main__':
     app.run()
